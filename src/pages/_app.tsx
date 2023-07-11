8001cd24ad728168ef4d33b9051cb41c96b5be43
Raamon <raamonpaiva@gmail.com>
2023-07-11 17:28:28 -0400

    Adicionado o Cabeçalho

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
-/** _app funciona como um wrapper para as páginas da aplicação */
+/** _app funciona como um wrapper para as páginas da aplicação, mesma função do App no create-react */
 import { globalStyle } from '@/styles/global'
+import { Container, Header } from '@/styles/pages/app'
 import type { AppProps } from 'next/app'
+import logoImg from '../assets/logo.svg'
 
 /** Aplicando os estilos globais */
 globalStyle()
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
