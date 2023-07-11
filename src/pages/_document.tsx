de514d11f417d7446272ade7d007eadd3891c1e5
Raamon <raamonpaiva@gmail.com>
2023-07-11 16:39:21 -0400

    Imported Roboto fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+/** O _document é como se fosse o index.html do create-react-app, porém as principais tags html, são componentes */
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/** É preciso mudar o crossorigin para crossOrigin e passar a propriedade */}
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
