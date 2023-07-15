51a61b412eeafa616f88725b8e6fa57b07c25f1a
Raamon <raamonpaiva@gmail.com>
2023-07-15 09:34:05 -0300

    Navegação Via Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import { stripe } from "@/lib/stripe";
 import { GetStaticProps } from "next";
 import Image from "next/image";
 import Stripe from "stripe";
+import Link from "next/link";
 
 
 interface HomeProps {
@@ -32,13 +33,15 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product  className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
