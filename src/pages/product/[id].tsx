3d160478339d5882e5ba6fc9c8e3b667ee181cf7
Raamon <raamonpaiva@gmail.com>
2023-07-17 18:35:35 -0400

    Carregando dados do produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,18 +1,32 @@
+import { stripe } from "@/lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
+import { GetStaticProps } from "next";
+import Image from "next/image";
+import Stripe from "stripe";
 
-export default function Product() {
+interface ProductProps {
+    id: string,
+    name: string,
+    imageUrl: string,
+    price: string,
+    description: string
+  }
+
+
+export default function Product(product: ProductProps) {
+
   return (
 
     <ProductContainer>
       <ImageContainer>
-
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
       </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi perspiciatis, illo laudantium facere voluptatum quos dignissimos illum eaque, voluptas exercitationem dolor animi enim ducimus quo maiores libero! Facere, quasi aliquam?</p>
+        <p>{product.description}</p>
 
         <button>
           Comprar Agora
@@ -20,4 +34,32 @@ export default function Product() {
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+
+  const productId = params.id
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  })
+
+  const price = product.default_price as Stripe.Price
+
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount as number / 100),
+        description: product.description
+      }
+    },
+    revalidade: 60 * 60 * 1 // 1 hour
+  }
 }
