154b46df8830cb0672d76edb49ce63affe008601
Raamon <raamonpaiva@gmail.com>
2023-07-17 15:13:21 -0400

    Estrutura do Produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -9,7 +9,14 @@ export default function Product() {
       </ImageContainer>
 
       <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
 
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi perspiciatis, illo laudantium facere voluptatum quos dignissimos illum eaque, voluptas exercitationem dolor animi enim ducimus quo maiores libero! Facere, quasi aliquam?</p>
+
+        <button>
+          Comprar Agora
+        </button>
       </ProductDetails>
     </ProductContainer>
   )
