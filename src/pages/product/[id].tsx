a85e3aded1b0a1ceee18683ac88d429475de4cd3
Raamon <raamonpaiva@gmail.com>
2023-07-18 13:49:49 -0400

    Fallback no SSG

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -2,6 +2,7 @@ import { stripe } from "@/lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import Stripe from "stripe";
 
 interface ProductProps {
@@ -16,6 +17,12 @@ interface ProductProps {
 
 
 export default function Product(product: ProductProps) {
+
+  const { isFallback } = useRouter()
+
+  if (isFallback) {
+    return <p>Loading ...</p>
+  }
 
   return (
 
@@ -43,10 +50,8 @@ export default function Product(product: ProductProps) {
  */
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
-    paths: [
-      { params: { id: 'prod_OGHjkDL0AqcapZ' } }
-    ],
-    fallback: false
+    paths: [],
+    fallback: true
   }
 }
 
