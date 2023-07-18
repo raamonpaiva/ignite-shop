b4ec518eb94f9788040786a47d3200178fab556c
Raamon <raamonpaiva@gmail.com>
2023-07-18 14:06:01 -0400

    Prefetch de Links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { stripe } from "@/lib/stripe";
 
 import { GetStaticProps } from "next";
 import Image from "next/image";
+import Link from "next/link";
 import Stripe from "stripe";
 
 
@@ -32,15 +33,15 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-
-          <Product href={`/product/${product.id}`} key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
-
+          <Link key={product.id} href={`/product/${product.id}`} prefetch={false}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
